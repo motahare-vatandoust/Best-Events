e42d9c5f79bbc26f6f347767f5d927f92b756be3
moti.vatandoust <moti.vatandoust@gmail.com>
2025-01-26 17:21:45 +0330

    Add image fetching functionality to EventForm

--- src/util/http.js
+++ src/util/http.js
@@ -1,13 +1,14 @@
 export async function fetchEvents({ signal, searchTerm }) {
-  let url = "http://localhost:3000/events";
+  let url = 'http://localhost:3000/events';
 
   if (searchTerm) {
-    url += "?search=" + searchTerm;
+    url += '?search=' + searchTerm;
   }
+
   const response = await fetch(url, { signal: signal });
 
   if (!response.ok) {
-    const error = new Error("An error occurred while fetching the events");
+    const error = new Error('An error occurred while fetching the events');
     error.code = response.status;
     error.info = await response.json();
     throw error;
@@ -18,22 +19,41 @@ export async function fetchEvents({ signal, searchTerm }) {
   return events;
 }
 
-export default async function createNewEvent(eventData) {
-  const response = fetch(`http://localhost:3000/events`, {
-    method: "POST",
+
+export async function createNewEvent(eventData) {
+  const response = await fetch(`http://localhost:3000/events`, {
+    method: 'POST',
     body: JSON.stringify(eventData),
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
   });
 
   if (!response.ok) {
-    const error = new Error("An error occurred while creating the event");
+    const error = new Error('An error occurred while creating the event');
     error.code = response.status;
     error.info = await response.json();
     throw error;
   }
 
-  const { event } = response.json();
+  const { event } = await response.json();
+
   return event;
 }
+
+export async function fetchSelectableImages({ signal }) {
+  const response = await fetch(`http://localhost:3000/events/images`, { signal });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while fetching the images');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { images } = await response.json();
+
+  return images;
+}
+
+export { fetchSelectableImages as default };
